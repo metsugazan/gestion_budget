e118f6df78c546d30e8e1ab269db308d1046c4ef
metsugazan <pas.mar@hotmail.fr>
2022-06-02 14:46:13 +0200

    repair tab for coop

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -5,14 +5,17 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 
 
-const Tab = createBottomTabNavigator
+const Tab = createBottomTabNavigator();
 
-const TabNavigation = () => (
-<NavigationContainer>
-    <Tab.Navigator>
-        <Tab.Screen name="Accueil" component={HomeScreen} />
-    </Tab.Navigator>
-</NavigationContainer>
-);
+const TabNavigation = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Accueil" component={HomeScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
 
-export default TabNavigation;
+
+export default TabNavigation; 
